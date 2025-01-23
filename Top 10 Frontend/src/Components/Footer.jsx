09e5489f7f29b5b9e7e9833f956d8f6c1a0ec57f
Nick Torrens <nicktorrens14@gmail.com>
2025-01-24 10:49:49 +1100

    changed EV for front to back end

--- Top 10 Frontend/src/Components/Footer.jsx
+++ Top 10 Frontend/src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer(){
     return(
         <footer className="footer">
-            <p>© 2025 Nick Torrens. All rights reserved. </p>
+            <p>© 2025 Nicholas Torrens. All rights reserved. </p>
         </footer>
     )
 }
