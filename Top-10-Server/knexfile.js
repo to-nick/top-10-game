817f2579bda81f6aed75de21cd9c679c6b1f4e13
Nick Torrens <nicktorrens14@gmail.com>
2025-01-24 11:10:22 +1100

    EV change 2

--- Top-10-Server/knexfile.js
+++ Top-10-Server/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     connection: {
       host: process.env.db_host,
       database: process.env.db_name,
-      user:     process.env.db_,
+      user:     process.env.db_user,
       password: process.env.db_password,
       port: process.env.db_port || 3306,
       ssl: {
